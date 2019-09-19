ab3defa22f298824cad3950a496f8569f1440c6e
a0123055u <m.thiruvalluvar@gmail.com>
2019-09-19 23:44:28 +0800

    added file processing line by line to avoid Memory replaced country by tagging for data processing using pycountry lib, stored noun_phrases in seprate files 	new file:   ReadME.txt 	modified:   extractWord.py 	new file:   pip_dependency_installed_list.PNG 	deleted:    test.txt

---
+++ ReadME.txt
@@ -0,0 +1,13 @@
+Before running the python program snippet please make sure
+
+1) python 2.7.16 installed with IDLE.
+2) Create a folde NLP and copy these two file
+3) test1.txt servers as input file.
+4) Try to install all the dependency shown in the pip_dependency_installed_list
+5) eg pip install nltk==3.4.5
+
+After the above steps are done
+1) Open the file using IDLE python tools and do a run module
+
+output:
+It will crreate output in (Person and organisation)noun.txt, country.txt(Country) and temp.text for corrected String Bites. 
--- extractWord.py
+++ extractWord.py
@@ -1,14 +1,36 @@
 from textblob import TextBlob
-txt = """Natural language processing (NLP) is a field of computer science, artificial intelligence, and computational linguistics concerned with the inter
-actions between computers and human (natural) languages."""
-f = open("test.txt", "r")
-print f.read()
-with open('test.txt', 'r') as file:
-    data = file.read().replace('\n', '')
-    blob = TextBlob(data)
-    print '########'
-    print 'length of noun phrases', len(blob.noun_phrases)
-    print 'length of blob' , len(blob)
-    print 'length of data', len(data)
-    print blob.noun_phrases
+import re
+import pycountry
+#replace symbols with empty string for easy NLP processing
+original_text = open('test1.txt').read()
+new_string = re.sub('[^a-zA-Z0-9\n\.]', ' ', original_text)
+open('temp.txt', 'w').write(new_string)
+
+#read line by line to aviod memory intense working of RAM
+filepath = 'temp.txt'
+
+with open(filepath) as fp:
+   line = fp.readline()
+   cnt = 1
+   while line:
+       print("Line {}".format(cnt))
+       line = fp.readline()
+       cnt += 1
+       blob = TextBlob(line)
+       print '##############################################'
+       print 'length of noun phrases', len(blob.noun_phrases)
+       print '##############################################'
+       noun = blob.noun_phrases
+       #write noun to file noun
+       open('noun.txt','a+').write(str(noun))
+       #need to process the above to take persons, organisation from the noun
+       for country in pycountry.countries:
+           if country.name in line:
+               #write the country to seperate file
+               line = line.replace(country.name,"<country> {} </country>".format(country.name))               
+               open('country.txt', 'a+').write(line)
+                         
+fp.close()
+
+
 
